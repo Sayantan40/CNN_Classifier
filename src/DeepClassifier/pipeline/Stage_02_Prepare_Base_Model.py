4d97766e03355ad0f34f386ac92bbf1378cbaba2
Sayantan40 <sayantanmitra.kol28@gmail.com>
2022-10-31 00:40:20 +0530

    updated prepare base model pipeline

--- src/DeepClassifier/pipeline/Stage_02_Prepare_Base_Model.py
+++ src/DeepClassifier/pipeline/Stage_02_Prepare_Base_Model.py
@@ -12,11 +12,13 @@ def main():
     prepare_base_model.update_base_model()
 
 if __name__ == '__main__':
+    
     try:
         logger.info(f"*******************")
         logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
         main()
         logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+    
     except Exception as e:
         logger.exception(e)
         raise e
