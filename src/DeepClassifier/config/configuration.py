17100f8aa106d359d3d566644e8274a07082068d
Sayantan40 <sayantanmitra.kol28@gmail.com>
2022-11-06 16:15:42 +0530

    evaluation stage updated

--- src/DeepClassifier/config/configuration.py
+++ src/DeepClassifier/config/configuration.py
@@ -1,7 +1,7 @@
 from DeepClassifier.constants import CONFIG_FILE_PATH,PARAMS_FILE_PATH
 from DeepClassifier.utils import  read_yaml,create_directories
 from DeepClassifier.entity.config_entity import DataIngestionConfig,PrepareBaseModelConfig
-from DeepClassifier.entity.config_entity import PrepareCallbacksConfig,TrainingConfig
+from DeepClassifier.entity.config_entity import PrepareCallbacksConfig,TrainingConfig,EvaluationConfig
 from pathlib import Path
 import os
 
@@ -94,23 +94,49 @@ class ConfigurationManager:
 
     def get_training_config(self) -> TrainingConfig:
         training = self.config.training
+        
         prepare_base_model = self.config.prepare_base_model
+        
         params = self.params
+        
         training_data = os.path.join(self.config.data_ingestion.unzip_dir, "PetImages")
         create_directories([
             Path(training.root_dir)
         ])
 
         training_config = TrainingConfig(
+            
             root_dir=Path(training.root_dir),
+            
             trained_model_path=Path(training.trained_model_path),
+            
             updated_base_model_path=Path(prepare_base_model.updated_base_model_path),
+            
             training_data=Path(training_data),
+            
             params_epochs=params.EPOCHS,
+            
             params_batch_size=params.BATCH_SIZE,
+            
             params_is_augmentation=params.AUGMENTATION,
+            
             params_image_size=params.IMAGE_SIZE
         )
 
         return training_config
+
+
+    
+    def get_validation_config(self) -> EvaluationConfig:
+        eval_config = EvaluationConfig(
+            
+            path_of_model=self.config.training.trained_model_path,
+            
+            training_data=self.config.data_ingestion.unzip_dir,
+            
+            params_image_size=self.params.IMAGE_SIZE,
+            
+            params_batch_size=self.params.BATCH_SIZE
+        )
+        return eval_config
 
--- src/DeepClassifier/entity/config_entity.py
+++ src/DeepClassifier/entity/config_entity.py
@@ -22,11 +22,14 @@ class PrepareBaseModelConfig:
     params_weights: str
     params_classes:int
 
+
+
 @dataclass(frozen=True)
 class PrepareCallbacksConfig:
     root_dir: Path
     tensorboard_root_log_dir: Path
     checkpoint_model_filepath: Path
+
 
 
 @dataclass(frozen=True)
@@ -39,4 +42,13 @@ class TrainingConfig:
     params_batch_size: int
     params_is_augmentation: bool
     params_image_size: list
+
+
+
+@dataclass(frozen=True)
+class EvaluationConfig:
+    path_of_model: Path
+    training_data: Path
+    params_image_size: list
+    params_batch_size: int
 
